31090ea20c7042c9d102ba7155feeb82e28d9c89
남주영 <skawndud321@gmail.com>
2018-09-18 00:24:06 +0900

    깃쓰기어렵다
    아아악

--- Drill/Drill4/character_moves.py
+++ Drill/Drill4/character_moves.py
@@ -7,5 +7,15 @@ character = load_image('character.png')
 
 # 여기를 채우세요.
 
+x=0
+while (x<800):
+    clear_canvas()
+    grass.draw(400,30)
+    character.draw(x,90)
+    x=x+2
+    update_canvas()
+    delay(0.001)
+    get_events()
+
 close_canvas()
 
--- Drill/Drill4/character_runs.py
+++ Drill/Drill4/character_runs.py
@@ -1,12 +1,65 @@
 from pico2d import *
+
 open_canvas()
+
 grass = load_image('grass.png')
+
 character = load_image('run_animation.png')
+
+character1 = load_image('animation_sheet.png')
 
 x = 0
 
 # 여기를 채우세요.
 
+x = 0
+
+frame = 0
+
+left = True
+
+while (True):
+
+    if (left):
+
+        while (x < 800):
+            clear_canvas()
+
+            grass.draw(400, 30)
+
+            character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+
+            update_canvas()
+
+            frame = (frame + 1) % 8
+
+            x += 5
+
+            delay(0.001)
+
+            get_events()
+
+    left = False
+
+    if (left == False):
+
+        while (x > 0):
+            clear_canvas()
+
+            grass.draw(400, 30)
+
+            character1.clip_draw(frame * 100, 0, 100, 100, x, 90)
+
+            update_canvas()
+
+            frame = (frame + 1) % 8
+
+            x -= 5
+
+            delay(0.001)
+
+            get_events()
+
+    left = True
 
 close_canvas()
-
