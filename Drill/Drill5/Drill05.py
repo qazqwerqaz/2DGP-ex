9bad373804f0d247efa3480cf044e0fccfd1fe01
남주영 <skawndud321@gmail.com>
2018-09-18 19:05:08 +0900

    Drill5 실습 완성
    실습완성 돌아오는 코드부분을 안만들고 있어서 마지막에 완성

--- Drill/Drill5/Drill05.py
+++ Drill/Drill5/Drill05.py
@@ -87,19 +87,121 @@ def move_d_to_e():
     pass
 
 def move_e_to_f():
+    ax, ay, bx, by = 715, 136, 316, 225
+    frame = 0
+    character.clip_draw(frame * 100, 0, 100, 100, ax, ay)
+    character.clip_draw(frame * 100, 0, 100, 100, bx, by)
+    delta_x = (ax - bx) // 10
+    delta_y = (ay - by) // 10
+    while (ax >= bx):
+        ax -= delta_x
+        ay -= delta_y
+        clear_canvas()
+        if delta_x > 0:
+            character.clip_draw(frame * 100, 0, 100, 100, ax, ay)
+        else:
+            character.clip_draw(frame * 100, 100, 100, 100, ax, ay)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.01)
     pass
 
 def move_f_to_g():
+    ax, ay, bx, by = 316, 225, 510, 92
+    frame = 0
+    character.clip_draw(frame * 100, 0, 100, 100, ax, ay)
+    character.clip_draw(frame * 100, 0, 100, 100, bx, by)
+    delta_x = (ax - bx) // 10
+    delta_y = (ay - by) // 10
+    while (ax <= bx):
+        ax -= delta_x
+        ay -= delta_y
+        clear_canvas()
+        if delta_x > 0:
+            character.clip_draw(frame * 100, 0, 100, 100, ax, ay)
+        else:
+            character.clip_draw(frame * 100, 100, 100, 100, ax, ay)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.01)
     pass
 
 def move_g_to_h():
-    pass
+    ax, ay, bx, by = 510,92,692,518
+    frame = 0
+    character.clip_draw(frame * 100, 0, 100, 100, ax, ay)
+    character.clip_draw(frame * 100, 0, 100, 100, bx, by)
+    delta_x = (ax - bx) // 10
+    delta_y = (ay - by) // 10
+    while (ax <= bx):
+        ax -= delta_x
+        ay -= delta_y
+        clear_canvas()
+        if delta_x > 0:
+            character.clip_draw(frame * 100, 0, 100, 100, ax, ay)
+        else:
+            character.clip_draw(frame * 100, 100, 100, 100, ax, ay)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.01)
+
 
 def move_h_to_i():
-    pass
+    ax, ay, bx, by = 692, 518, 682, 336
+    frame = 0
+    character.clip_draw(frame * 100, 0, 100, 100, ax, ay)
+    character.clip_draw(frame * 100, 0, 100, 100, bx, by)
+    delta_x = (ax - bx) // 10
+    delta_y = (ay - by) // 10
+    while (ax >= bx):
+        ax -= delta_x
+        ay -= delta_y
+        clear_canvas()
+        if delta_x > 0:
+            character.clip_draw(frame * 100, 0, 100, 100, ax, ay)
+        else:
+            character.clip_draw(frame * 100, 100, 100, 100, ax, ay)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.01)
 
 def move_i_to_j():
-    pass
+    ax, ay, bx, by = 682, 336, 712, 349
+    frame = 0
+    character.clip_draw(frame * 100, 0, 100, 100, ax, ay)
+    character.clip_draw(frame * 100, 0, 100, 100, bx, by)
+    delta_x = (ax - bx) // 10
+    delta_y = (ay - by) // 10
+    while (ax <= bx):
+        ax -= delta_x
+        ay -= delta_y
+        clear_canvas()
+        if delta_x > 0:
+            character.clip_draw(frame * 100, 0, 100, 100, ax, ay)
+        else:
+            character.clip_draw(frame * 100, 100, 100, 100, ax, ay)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.01)
+
+def move_i_to_a():
+    ax, ay, bx, by = 682, 336, 203, 535
+    frame = 0
+    character.clip_draw(frame * 100, 0, 100, 100, ax, ay)
+    character.clip_draw(frame * 100, 0, 100, 100, bx, by)
+    delta_x = (ax - bx) // 10
+    delta_y = (ay - by) // 10
+    while (ax >= bx):
+        ax -= delta_x
+        ay -= delta_y
+        clear_canvas()
+        if delta_x > 0:
+            character.clip_draw(frame * 100, 0, 100, 100, ax, ay)
+        else:
+            character.clip_draw(frame * 100, 100, 100, 100, ax, ay)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.01)
 while True:
     move_a_to_b()
     move_b_to_c()
@@ -110,5 +212,6 @@ while True:
     move_g_to_h()
     move_h_to_i()
     move_i_to_j()
+    move_i_to_a()
 
 close_canvas()
