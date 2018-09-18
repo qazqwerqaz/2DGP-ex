20c37b151db3087eb274f836ce7b9ec9e54ae2c9
남주영 <skawndud321@gmail.com>
2018-09-18 18:34:21 +0900

    Drill5 실습 구조 완성
    코드 구조 완성

--- Drill/Drill5/Drill05.py
+++ Drill/Drill5/Drill05.py
@@ -6,5 +6,40 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 # fill here
+def move_a_to_b():
+    pass
+
+def move_b_to_c():
+    pass
+
+def move_c_to_d():
+    pass
+
+def move_d_to_e():
+    pass
+
+def move_e_to_f():
+    pass
+
+def move_f_to_g():
+    pass
+
+def move_g_to_h():
+    pass
+
+def move_h_to_i():
+    pass
+
+def move_i_to_j():
+    pass
+while True:
+    move_a_to_b()
+    move_b_to_c()
+    move_c_to_d()
+    move_e_to_f()
+    move_f_to_g()
+    move_g_to_h()
+    move_h_to_i()
+    move_i_to_j()
 
 close_canvas()
