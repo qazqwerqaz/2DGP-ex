496d38c2cf0fa3994c15166f834bfbf4e3ba3716
남주영 <skawndud321@gmail.com>
2018-09-20 18:34:12 +0900

    Drill6 마우스 커서
    마우스커서 및 이동 변수 선언

--- Drill/Drill6/move_character_with_mouse.py
+++ Drill/Drill6/move_character_with_mouse.py
@@ -6,13 +6,17 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 def handle_events():
     # fill here
     global running
+    global mouse_x, mouse_y
+    global dir_x, dir_y
     global x, y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
+            mouse_x, mouse_y = event.x, KPU_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+           
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -26,14 +30,17 @@ hand_arrow = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+mouse_x, mouse_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+dir_x, dir_y = 0, 0
 frame = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    hand_arrow.draw(x,y)
-    #character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    hand_arrow.draw(mouse_x, mouse_y)
+    if mouse_x > x:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
     frame = (frame + 1) % 8
 
