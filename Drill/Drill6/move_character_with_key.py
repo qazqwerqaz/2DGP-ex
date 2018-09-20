4099ead50305acca63626e127637bd68d831d045
남주영 <skawndud321@gmail.com>
2018-09-20 18:26:37 +0900

    Drill 06 실습시작
    필요한 함수 생성

--- Drill/Drill6/move_character_with_key.py
+++ Drill/Drill6/move_character_with_key.py
@@ -3,6 +3,25 @@ from pico2d import *
 
 def handle_events():
     # fill here
+    global running
+    global x
+    global dir
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
     pass
 
 
@@ -13,13 +32,26 @@ character = load_image('animation_sheet.png')
 running = True
 x = 800 // 2
 frame = 0
-
+dir = 0
+view = 0
 while running:
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
-    update_canvas()
+    if dir == 1:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
+        view = 1
+    elif dir == -1:
+        character.clip_draw(frame * 100, 100 * 0, 100, 100, x, 90)
+        view = 0
+    elif view == 1:
+        character.clip_draw(frame * 100, 100 * 3, 100, 100, x, 90)
+    elif view == 0:
+        character.clip_draw(frame * 100, 100 * 2, 100, 100, x, 90)
 
+
+
+    update_canvas()
+    x += dir * 5
     handle_events()
     frame = (frame + 1) % 8
 
