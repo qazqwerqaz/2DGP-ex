864f99ec3cc31f1fa1b49c5151f84e2f6c9b522b
남주영 <skawndud321@gmail.com>
2018-10-02 19:00:52 +0900

    Drill7 캐릭터출력 완료
    캐릭터 출력완료

--- Drill/Drill7/line.py
+++ Drill/Drill7/line.py
@@ -7,16 +7,17 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def draw_line(p1, p2):
     # fill here
+    global character
+    global frame
 
-    frame = 0
-    character.clip_draw(frame * 100, 100 * 3, 100, 100, p1, p2)
-
-    for i in range(0, 100+1, 10):
+    global x
+    global y
+    for i in range(0, 100+1, 1):
         t = i / 100
         x = (1-t)*p1[0]+t*p2[0]
         y = (1-t)*p1[1]+t*p2[1]
-        #draw_point((x,y))
-        frame = (frame + 1) % 8
+
+
 
     #draw_point(p2)
     pass
@@ -24,7 +25,8 @@ def draw_line(p1, p2):
 # fill here
 
 size = 10
-points = [(random.randint(-500, 500), random.randint(-350, 350)) for i in range(size)]
+
+points = [(random.randint(0, 800), random.randint(0, 800)) for i in range(size)]
 
 n = 1
 # fill here
@@ -32,11 +34,17 @@ open_canvas(KPU_WIDTH,KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
+frame = 0
 
+x = 0
+y = 0
 while True:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-   # draw_line(points[n-1], points[n])
+    draw_line(points[n-1], points[n])
+
+    character.clip_draw(frame * 100, 100 * 3, 100, 100, x, y)
+    frame = (frame + 1) % 8
     n = (n+1) % size
 
     update_canvas()
