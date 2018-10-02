d2683d5d9a0bdd8e17dc3b3dc45f536f7b6197f6
남주영 <skawndud321@gmail.com>
2018-10-02 18:52:43 +0900

    Drill 7 배경 출력
    배경출력 및 이동함수구현 완료

--- Drill/Drill7/line.py
+++ Drill/Drill7/line.py
@@ -1,67 +1,50 @@
-import turtle
+from pico2d import *
 import random
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
-def stop():
-    turtle.bye()
-
-
-def prepare_turtle_canvas():
-    turtle.setup(1024, 768)
-    turtle.bgcolor(0.2, 0.2, 0.2)
-    turtle.penup()
-    turtle.hideturtle()
-    turtle.shape('arrow')
-    turtle.shapesize(2)
-    turtle.pensize(5)
-    turtle.color(1, 0, 0)
-    turtle.speed(100)
-    turtle.goto(-500, 0)
-    turtle.pendown()
-    turtle.goto(480, 0)
-    turtle.stamp()
-    turtle.penup()
-    turtle.goto(0, -360)
-    turtle.pendown()
-    turtle.goto(0, 360)
-    turtle.setheading(90)
-    turtle.stamp()
-    turtle.penup()
-    turtle.home()
-
-    turtle.shape('circle')
-    turtle.pensize(1)
-    turtle.color(0, 0, 0)
-    turtle.speed(50)
-
-    turtle.onkey(stop, 'Escape')
-    turtle.listen()
-
-
-def draw_big_point(p):
-    turtle.goto(p)
-    turtle.color(0.8, 0.9, 0)
-    turtle.dot(15)
-    turtle.write('     '+str(p))
-
-
-def draw_point(p):
-    turtle.goto(p)
-    turtle.dot(5, random.random(), random.random(), random.random())
-
-
-def draw_line_basic(p1, p2):
-    # fill here
-    pass
 
 
 def draw_line(p1, p2):
     # fill here
+
+    frame = 0
+    character.clip_draw(frame * 100, 100 * 3, 100, 100, p1, p2)
+
+    for i in range(0, 100+1, 10):
+        t = i / 100
+        x = (1-t)*p1[0]+t*p2[0]
+        y = (1-t)*p1[1]+t*p2[1]
+        #draw_point((x,y))
+        frame = (frame + 1) % 8
+
+    #draw_point(p2)
     pass
-
-
-prepare_turtle_canvas()
 
 # fill here
 
-turtle.done()
+size = 10
+points = [(random.randint(-500, 500), random.randint(-350, 350)) for i in range(size)]
+
+n = 1
+# fill here
+open_canvas(KPU_WIDTH,KPU_HEIGHT)
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
+
+
+while True:
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+   # draw_line(points[n-1], points[n])
+    n = (n+1) % size
+
+    update_canvas()
+
+
+
+close_canvas()
+
+
+
+
