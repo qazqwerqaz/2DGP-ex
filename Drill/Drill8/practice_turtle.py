3d2e26a632309e106deb5da1e580c092398397cb
남주영 <skawndud321@gmail.com>
2018-10-04 18:33:15 +0900

    drill8 거북이 완료
    거북이완료

--- Drill/Drill8/practice_turtle.py
+++ Drill/Drill8/practice_turtle.py
@@ -76,13 +76,6 @@ def draw_curve_4_points(p1, p2, p3, p4):
     draw_big_point(p3)
     draw_big_point(p4)
 
-    # draw p1-p2
-    for i in range(0, 50, 2):
-        t = i / 100
-        x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
-        y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
-        draw_point((x, y))
-    draw_point(p2)
 
     # draw p2-p3
     for i in range(0, 50, 2):
@@ -107,15 +100,14 @@ def draw_curve_5_points(p1, p2, p3, p4):
     draw_big_point(p3)
     draw_big_point(p4)
 
-
-
-    # draw p3-p4
-    for i in range(50, 100, 2):
-        t = i / 100
-        x = (2*t**2-3*t+1)*p2[0]+(-4*t**2+4*t)*p3[0]+(2*t**2-t)*p4[0]
-        y = (2*t**2-3*t+1)*p2[1]+(-4*t**2+4*t)*p3[1]+(2*t**2-t)*p4[1]
+    for i in range(0, 50, 2):
+        t = i / 50
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
         draw_point((x, y))
-    draw_point(p4)
+    draw_point(p3)
 
 
 
@@ -123,9 +115,10 @@ def draw_curve_5_points(p1, p2, p3, p4):
 
 prepare_turtle_canvas()
 
-
-draw_curve_4_points((-300,200),(400,350),(300, -300) ,(-200,-200))
-draw_curve_5_points((-200,-200),(300, -300) ,(-200,-200), (-300, 200))
-
+while True:
+    draw_curve_5_points((-300,200),(400,350),(300, -300) ,(-200,-200))
+    draw_curve_5_points((400,350),(300,-300),(-200, -200), (-300,200))
+    draw_curve_5_points((300, -300) ,(-200,-200), (-300, 200),(400,350))
+    draw_curve_5_points((-200, -200),(-300, 200), (400, 350), (300,-300))
 
 turtle.done()
