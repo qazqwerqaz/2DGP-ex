e1b4e7c13589feca019da14d5c47814b873cb29f
남주영 <skawndud321@gmail.com>
2018-10-04 19:02:30 +0900

    Drill8 완료
    2017180009 남주영
    커밋횟수 5회https://youtu.be/QF_1NeVxuD8
    https://youtu.be/ZxYN7GKlpCE

--- Drill/Drill8/practice_KPU.py
+++ Drill/Drill8/practice_KPU.py
@@ -14,19 +14,19 @@ def draw_line(p1, p2, p3, p4):
     global y
     global i
 
-    for i in range(0, 50, 2):
-        t = i / 50
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
+    t=0
+    t = i / 100
+    x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
+    y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
-
+    i=i+1
 
     pass
 
 # fill here
 
-size = 10
+size = 20
 
 points = [(random.randint(400, 800), random.randint(400, 800)) for i in range(size)]
 
@@ -42,16 +42,21 @@ frame = 0
 x = 0
 y = 0
 i = 0
+
 while True:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    draw_line(points[n-1], points[n],points[n+1],points[n+2])
+    draw_line(points[n], points[(n+1)%size+1],points[(n+2)%size+1],points[(n+3)%size+1])
 
     character.clip_draw(frame * 100, 100 * 3, 100, 100, x, y)
+    for a in range(0, n, 1):
+
+        character.clip_draw(1 * 100, 100 * 3, 100, 100, points[a+3][0],points[a+3][1])
     frame = (frame + 1) % 8
     if i == 100:
-        n = (n+1) % size
+        n = (n+1) % 16
         i=0
+
 
     update_canvas()
 
