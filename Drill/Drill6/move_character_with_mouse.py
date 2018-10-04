cf5271be2ee5d979fd578e7f4b77a3b9b6febd2c
남주영 <skawndud321@gmail.com>
2018-10-04 16:49:25 +0900

    Drill8 수업 시작
    시작

--- Drill/Drill6/move_character_with_mouse.py
+++ Drill/Drill6/move_character_with_mouse.py
@@ -1,73 +1,48 @@
 from pico2d import *
-import math
+import random
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
-def handle_events():
+
+
+def draw_line(p1, p2):
     # fill here
-    global running
-    global mouse_x, mouse_y
-    global stop_mouse_x, stop_mouse_y
-    global dir_x, dir_y
-    global x, y
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_MOUSEMOTION:
-            mouse_x, mouse_y = event.x + 17, KPU_HEIGHT - 1 - event.y - 20
-        elif event.type == SDL_MOUSEBUTTONDOWN:
-            dir_x = 0
-            dir_y = 0
-            stop_mouse_x = event.x
-            stop_mouse_y = KPU_HEIGHT - 1 - event.y
-            dir_x = (float)(event.x - x)//20
-            dir_y = (float)(KPU_HEIGHT - 1 - event.y - y)//20
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+
+    frame = 0
+    character.clip_draw(frame * 100, 100 * 3, 100, 100, p1, p2)
+
+    for i in range(0, 100+1, 10):
+        t = i / 100
+        x = (1-t)*p1[0]+t*p2[0]
+        y = (1-t)*p1[1]+t*p2[1]
+        #draw_point((x,y))
+        frame = (frame + 1) % 8
+
+    #draw_point(p2)
     pass
 
+# fill here
 
+size = 10
+points = [(random.randint(-500, 500), random.randint(-350, 350)) for i in range(size)]
+
+n = 1
 # fill here
 open_canvas(KPU_WIDTH,KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 hand_arrow = load_image('hand_arrow.png')
 
-running = True
-x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
-mouse_x, mouse_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
-dir_x, dir_y = 0.0, 0.0
-stop_mouse_x, stop_mouse_y = 0,0
-frame = 0
-hide_cursor()
 
-while running:
+while True:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    hand_arrow.draw(mouse_x, mouse_y)
-    if dir_x == 0:
-        character.clip_draw(frame * 100, 100 * 3, 100, 100, x, y)
-    elif dir_x < 0:
-        character.clip_draw(frame * 100, 100 * 0, 100, 100, x, y)
-    elif dir_x > 0:
-        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-
+   # draw_line(points[n-1], points[n])
+    n = (n+1) % size
 
     update_canvas()
-    frame = (frame + 1) % 8
 
-    x += dir_x
-    y += dir_y
 
-    if 10 >= math.fabs(stop_mouse_x - x):
-        dir_x = 0
-        dir_y = 0
-        x = stop_mouse_x
-        y = stop_mouse_y
-
-    delay(0.02)
-    handle_events()
 
 close_canvas()
 
